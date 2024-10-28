681ced34eb996776a633baddf0388768ae2e7d36
Elena Riso <risoelena87@gmail.com>
2024-10-28 19:45:43 -0400

    day02

---
+++ tests/day02/playwright-practice.spec.js
@@ -0,0 +1,22 @@
+import { test } from '@playwright/test';
+
+test("Search Youtube @youtube2", async ({ page }) => {
+  // Your test steps go here
+// navigate to youtube
+await page.goto("https://www.youtube.com");
+
+let searchBox= page.locator("//input[@id='search']");
+
+searchBox.click();
+
+//await searchBox.type("CYDEO");
+await searchBox.fill("Cydeo");
+
+await page.waitForTimeout(3000);
+
+await searchBox.press("Enter");
+
+await page.waitForTimeout(3000);
+
+
+});
